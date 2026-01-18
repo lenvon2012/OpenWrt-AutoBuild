f271d71088e249279bc74d96252762654f7b03ef
lenvon2012 <lenvon2012@qq.com>
2026-01-18 18:47:25 +0800

    update

--- build/immortalwrt_x86_64/custom.sh
+++ build/immortalwrt_x86_64/custom.sh
@@ -30,7 +30,7 @@ ZZZ="package/emortal/default-settings/files/99-default-settings"
 
 #
 sed -i "s#192.168.1.1#1192.168.10.1#g" $NET                                                     # 定制默认IP
- sed -i "s#ImmortalWrt#OpenWrt#g" $NET                                          # 修改默认名称为 ImmortalWrt-X86
+sed -i "s#ImmortalWrt#OpenWrt#g" $NET                                          # 修改默认名称为 ImmortalWrt-X86
 # sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' $ZZZ                                          # 取消系统默认密码
 echo "uci set luci.main.mediaurlbase=/luci-static/argon" >> $ZZZ                      # 设置默认主题(如果编译可会自动修改默认主题的，有可能会失效)
 
